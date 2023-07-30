546b65e3e76903499fb7e791c6e8ab903453cb8d
D. Prameswara <meshwara@gmail.com>
2023-07-30 11:50:21 +0700

    Initial Commit

---
+++ FAB/FABApp.swift
@@ -0,0 +1,17 @@
+//
+//  FABApp.swift
+//  FAB
+//
+//  Created by D. Prameswara on 30/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FABApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
